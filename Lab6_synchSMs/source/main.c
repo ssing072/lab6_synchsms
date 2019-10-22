4eef9483abc14ff0a311e60a12c2f0a1f60e5b6c
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-22 12:07:47 -0700

    Update main.c

--- Lab6_synchSMs/source/main.c
+++ Lab6_synchSMs/source/main.c
@@ -8,16 +8,51 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <arv/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn(){
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSIK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;  
+}
+    
+void TimerOff(){
+    TCCR1B = 0x00;
+}
+
+void TimerISR(){
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRC = 0xFF;
+    PORTC = 0x00;
+    TimerSet(1000);
+    TimerOn();
+    unsigned char tmpB = 0x00;
     /* Insert your solution below */
     while (1) {
-
+        tmpB = ~tmpB;
+        PORTC = tmpB;
+        while(!TimerFlag);
+        TimerFlag = 0'
     }
     return 1;
 }
